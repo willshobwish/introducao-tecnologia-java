b5a5bb05b665c86d39860a777bacd6603961da46
Willian Murayama <willianyos13@gmail.com>
2023-04-03 19:48:56 -0300

    Created exercise 05

---
+++ Aulas/Aula05Exercicio/src/Model/Livro.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Willian Murayama
+ */
+public class Livro {
+
+    private String nome;
+    private String descricao;
+    private int quantidade;
+
+    public Livro(String nome, String descricao, int quantidade) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
