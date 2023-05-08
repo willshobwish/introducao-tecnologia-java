a2dd0b900dbb43231b9cd2136270ed6c1c0041eb
Willian Murayama <willianyos13@gmail.com>
2023-05-08 20:11:35 -0300

    Create atividade banco de dados

---
+++ Aulas/2023-05-08-Atividade/src/main/java/com/mycompany/atividade/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade;
+
+/**
+ *
+ * @author willm
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
